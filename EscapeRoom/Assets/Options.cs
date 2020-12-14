3978c39d22fc09486646f379f49c5dfd70c9d594
kishooo <47245011+kishooo@users.noreply.github.com>
2020-12-15 01:47:44 +0200

    main menu added

---
+++ EscapeRoom/Assets/Options.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Options : MonoBehaviour
+{
+    private bool isMute;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMute = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Back()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void isMutePressed()
+    {
+        isMute = !isMute;
+        AudioListener.pause = isMute;    
+    }
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+    public void Credits()
+    {
+        SceneManager.LoadScene("credits");
+    }
+}
